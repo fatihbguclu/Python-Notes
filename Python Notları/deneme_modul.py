121f6c5af0a8f30a1123cff3b0f221bf87325e69
fatihbguclu <45896097+fatihbguclu@users.noreply.github.com>
2019-11-28 21:56:36 +0300

    Add files

---
+++ Python Notları/deneme_modul.py
@@ -0,0 +1,28 @@
+
+# -*- coding:cp1254 -*-
+
+print("asdqwe")
+liste = [1,2,3,4]
+a = 42
+b = 1
+metin = "şçöiğ"
+
+def func(inp):
+    print("input = " + str(inp))
+
+
+class sinif:
+    sinif_var1 = 20.0
+    sinif_var2 = "metin"
+
+    def kib(self):
+        print("kendine iyi bak")
+    
+    def selam(self,isim):
+        print("Merhaba " + isim)
+
+if(__name__ == "__main__"):
+    print("deneme")
+    print("test baslangic")
+    func("inp")
+    print("test bitis")
